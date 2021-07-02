fbd7bfeece85cd6bc024ff666c4b824835ffb29a
sssoz <ouchsophy@gmail.com>
2021-07-02 18:26:42 +0100

    Fix msg when form is submitted successfully

--- src/js/newsletter.js
+++ src/js/newsletter.js
@@ -1,11 +1,5 @@
 jQuery(document).ready(function() {
 
-  // Display thank you message on form submit
-  function showSuccessMsg() {
-    var newsletterThanks = "<p class='type-1 fw-900'>Thanks for signing up! We will be in touch.</p>";
-    document.getElementById("newsletter").innerHTML = newsletterThanks;
-  }
-
   var paramsintoform = function() {
     if (window.location.search) {
       var s = window.location.search.split('?')[1];
@@ -23,6 +17,12 @@ jQuery(document).ready(function() {
 
   var goto = false;
   var valid = undefined;
+  var showMessage = function(e) {
+    // display thank you text on success
+    var newsletterThanks = "<p class='type-1 fw-900'>Thanks for signing up! We will be in touch.</p>";
+    document.getElementById("newsletter").innerHTML = newsletterThanks;
+  };
+
   var submit = function(e) {
     if (valid === false) {
       valid = undefined; // reset validity check and let the form continue to display warning
@@ -35,9 +35,6 @@ jQuery(document).ready(function() {
       } else {
         // form valid now, send the data and go to thanks
         $('#submit').replaceWith('<button>Submitting&hellip;</button>');
-
-        // display thank you text
-        showSuccessMsg();
 
         var data = {};
 
@@ -63,13 +60,14 @@ jQuery(document).ready(function() {
           data: data,
           success: function(res) {
             console.log(res);
+            showMessage();
             if (window.location.search.indexOf('nothanks') === -1) window.location = goto;
           }
         });
       }
     }
   }
-  $('body').on('click','#submit',submit);
+  $('#newsletter').on('click','#submit', submit);
 });
 
 // Above script supports many forms, the script below points it to the right sheet
@@ -78,7 +76,7 @@ url = 'https://api.openaccessbutton.org/ill/collect/' + sid;
 fields = [];
 
   // Allow the enter key to be used to submit while in the text box
-  var input = document.getElementById("signup");
+  var input = document.getElementById("email");
   // Execute a function when the user releases a key on the keyboard
   input.addEventListener("keyup", function(event) {
     // Number 13 is the "Enter" key on the keyboard
